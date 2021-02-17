f48e8aba19a538bdf2800f612029d117b11404e5
MohanKrishnaDevOps <72590354+MohanKrishnaDevOps@users.noreply.github.com>
2021-02-17 12:19:52 +0530

    Add files via upload

--- src/main/java/com/beta/replyservice/StringReplyService.java
+++ src/main/java/com/beta/replyservice/StringReplyService.java
@@ -35,46 +35,46 @@ public class StringReplyService
 	public static void main(String[] args) 
 	{
 		Scanner sc= new Scanner(System.in);
-		while(app == true)
+		while(true)
 		{
-			System.out.print("String is kbzw9ru");
-			System.out.print("1. /reply/kbzw9ru");
-			System.out.print("2. reverse the string");
-			System.out.print("3. encode the string via MD5 hash algorithm");
-			System.out.print("4. /reply/11-kbzw9ru");
-			System.out.print("5. /reply/12-kbzw9ru");
+			System.out.println("String is kbzw9ru");
+			System.out.println("1. /reply/kbzw9ru");
+			System.out.println("2. reverse the string");
+			System.out.println("3. encode the string via MD5 hash algorithm");
+			System.out.println("4. /reply/11-kbzw9ru");
+			System.out.println("5. /reply/12-kbzw9ru");
 			int a= sc.nextInt();  
 			if(a == 1)
 			{  
-				System.out.print("String is kbzw9ru"); 
+				System.out.println("String is kbzw9ru"); 
 			}else if(a == 2)
 			{  
-				System.out.print("reverse the string is ", + reverseString(kbzw9ru));  
+				System.out.println("reverse the string is " + reverseString("kbzw9ru"));  
 			}  
 			else if(a == 3)
 			{  
-				System.out.print("encode the string via MD5 hash algorithm", + getMd5(kbzw9ru));  
+				System.out.println("encode the string via MD5 hash algorithm    " + getMd5("kbzw9ru"));  
 			}    
 			else if(a == 4)
 			{  
-				System.out.print("String is kbzw9ru");
+				System.out.println("String is kbzw9ru");
 			} 
 			else if(a == 5)
 			{  
-				System.out.print("/reply/11-kbzw9ru", + getMd5(reverseString(kbzw9ru)));
+				System.out.println("/reply/11-kbzw9ru" + getMd5(reverseString("kbzw9ru")));
 			} 
 			else 
 			{  
-				System.out.print("Please select correct option");
+				System.out.println("Please select correct option");
 				continue;
 			} 
-			System.out.print("Do you want to continue?");
-			System.out.print("if yes - y  no - n");
+			System.out.println("Do you want to continue?");
+			System.out.println("if yes - y  no - n");
 			String str= sc.nextLine(); 
-			if(str == y || str == Y)
+			if(str == "y" || str == "Y")
 			{
 				continue;
-			}else if(str == n || str == N) 
+			}else if(str == "n" || str == "N") 
 			{
 				break;
 			}else
